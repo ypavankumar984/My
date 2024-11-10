c2897f13916abc60fa8c731bb9e81c9603eac818
ypavankumar984 <y.pavankumar9849@gmail.com>
2024-11-10 12:38:53 +0530

    added all files

---
+++ app/src/main/java/com/example/my/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.my;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the TextView in the layout and set its text to "Hello, World!"
+        TextView helloTextView = findViewById(R.id.helloTextView);
+        helloTextView.setText("Hello, World!");
+    }
+}
