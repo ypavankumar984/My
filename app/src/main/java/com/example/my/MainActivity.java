d06318da0e1bf258d3b71901d0339359a6a2e39a
ypavankumar984 <y.pavankumar9849@gmail.com>
2024-11-10 13:28:32 +0530

    added master rey 123

--- app/src/main/java/com/example/my/MainActivity.java
+++ app/src/main/java/com/example/my/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//master rey 123
         // Find the TextView in the layout and set its text to "Hello, World!"
         TextView helloTextView = findViewById(R.id.helloTextView);
-        helloTextView.setText("Hello, World!");
+        helloTextView.setText("Roshan123!");
     }
 }
